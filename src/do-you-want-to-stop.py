f8ee46d528ae8667894f743e6f358141a2080343
Silas-tander <silas@tanderup.net>
2022-09-05 17:44:43 +0200

    second update not stopping

--- src/do-you-want-to-stop.py
+++ src/do-you-want-to-stop.py
@@ -2,9 +2,6 @@
 # You should write a loop around this question, and keep asking until
 # the answer is "yes" (lower case).
 
-n = input("Do you want me to stop?")
+while(n != "yes"):
+    n = input('Do you want me to stop?')
 
-while != "yes":
-    n
-    if n == "yes":
-        break
