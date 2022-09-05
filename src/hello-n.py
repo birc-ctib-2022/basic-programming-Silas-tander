595dc419fe25ca1ff201cc61a4cb9daa97d7b7a4
Silas-tander <silas@tanderup.net>
2022-09-05 17:24:12 +0200

    Hello_n_py_unspecified

--- src/hello-n.py
+++ src/hello-n.py
@@ -2,9 +2,10 @@
 # this asks the user for a number and turns the answer
 # into an integer
 
+n = int(input("How many times should I loop? "))
 
 # Now, print "Hello, World!" n times.
-for x in range(5):
+for x in range(n):
     print("Hello, World!")
 
 
